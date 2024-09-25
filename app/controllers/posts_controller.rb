1d23016f7c9c12aac8bc54fc8e1272fc5c48cfbb
rsnaka <166614766+rsnaka@users.noreply.github.com>
2024-09-25 11:53:53 +0000

    一覧

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
 
+  def new
+    @new_post = Post.new
+  end
+
+  def create
+    @new_post = Post.new(post_params)
+    @new_post.user_id = current_user.id
+    @new_post.save
+    redirect_to post_images_path
+  end
+
   def index
-    @posts = Posts.all
+    @posts = Post.index
+  end
+
+  def show
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def posts
+    params.require(:new_posts).permit(:post_content, :image)
   end
 
 end
